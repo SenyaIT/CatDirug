6e81814280aec5f9797a588897b2b2913db2a816
Arsenii Skudaev <arseniyskudaev@ad.hms.org.il>
2025-05-15 14:15:29 +0300

    fixed path

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ import { Cat } from "./cat";
 
 const Doc = document.getElementById("app")
 
-Cat1 = new Cat("Simon", 9, "Russia", "./catPhotos/Simon.jpg")
-Doc.appendChild(Cat1);
+Cat1 = new Cat("Simon", 9, "Russia", './catPhotos/Simon.jpg')
+Doc.appendChild(Cat1); 
